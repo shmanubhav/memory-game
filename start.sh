e06b35ed3cb8e1d29c914dac21fdc88992b99477
Anubhav Sharma <sharma.an@husky.neu.edu>
2018-09-27 17:57:39 -0400

    fixed html

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ export PORT=4791
 
 echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/memory/bin/memory stop || true
+$(pwd)/_build/prod/rel/memory/bin/memory stop || true
 
 echo "Starting app..."
 
 # TODO: You want start.
 
-_build/prod/rel/memory/bin/memory start
+$(pwd)/_build/prod/rel/memory/bin/memory start
 
 # TODO: Add a cron rule or systemd service file
 #       to start your app on system boot.
